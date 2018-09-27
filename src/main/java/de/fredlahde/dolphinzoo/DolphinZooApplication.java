32b73582b2ab31259e1a5ce1b2adb1d10b59b354
fred <fredlahde@gmail.com>
2018-09-27 22:30:54 +0200

    disable jpa autoconfiguration

---
+++ src/main/java/de/fredlahde/dolphinzoo/DolphinZooApplication.java
@@ -0,0 +1,19 @@
+package de.fredlahde.dolphinzoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class
+})
+public class DolphinZooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DolphinZooApplication.class, args);
+    }
+}
